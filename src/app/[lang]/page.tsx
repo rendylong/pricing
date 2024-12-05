e543c0751b58d65034554b7409a12fd8e572cf25
rendylong <didobang@foxmail.com>
2024-12-05 19:34:11 +0800

    v5.6

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,17 +1,5 @@
-'use client'
+import { HomePage } from '@/components/pages/HomePage'
 
-import { PricingCalculator } from '@/components/pricing/Calculator'
-import { Header } from '@/components/layout/Header'
-
-export default function PricingPage({ params }: { params: { lang: string } }) {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <main className="pt-8 pb-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <PricingCalculator lang={params.lang} />
-        </div>
-      </main>
-    </div>
-  )
+export default function Page() {
+  return <HomePage />
 } 
